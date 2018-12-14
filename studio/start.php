899bb4582f02093c35416c23c030afb7879c7163
Observer KRypt0n_ <suimin.tu.mu.ga.mi@gmail.com>
2018-12-14 17:53:18 +0200

    Фикс небольшого изменения - раскоментированы строчки кода для нормального старта среды VoidStudio

--- studio/start.php
+++ studio/start.php
@@ -5,10 +5,10 @@ namespace VoidEngine;
 const WORKSPACE = __DIR__;
 
 require __DIR__ .'/VoidStudio API.php';
-//require __DIR__ .'/forms/load.php';
+require __DIR__ .'/forms/load.php';
 require __DIR__ .'/forms/main.php';
 
-//VoidStudioAPI::getObjects ('load')['MainForm']->showDialog ();
+VoidStudioAPI::getObjects ('load')['MainForm']->showDialog ();
 VoidStudioAPI::getObjects ('main')['MainForm']->showDialog ();
 
 ?>
